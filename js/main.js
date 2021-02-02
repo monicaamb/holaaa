d12bef794c3305b885b2ff38e0ad40030dce60f6
Mónica <monicaamb11@gmail.com>
2021-02-02 01:31:11 +0100

    Añadido autoplay

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
 // Variables
+const imagenes = ['img/foto1.jpg', 'img/foto2.jpg', 'img/foto3.jpg'];
+let pagina = 1;
 const objetoImg = document.querySelector('#imagen');
 const botonAvanzar = document.querySelector('#avanzar');
 const botonRetroceder = document.querySelector('#retroceder');
 const templateCirculo = document.querySelector('#template-circulo').content.firstElementChild;
 const circulos = document.querySelector('#circulos');
-const botonParar  = document.querySelector('#parar');
-const botonAutoPlay  = document.querySelector('#autoplay');
+const botonParar = document.querySelector('#parar');
+const botonAutoplay = document.querySelector('#autoplay');
 let intervalo = null;
 const tiempoIntervaloSeg = 1;
-const imagenes = ['img/foto1.jpg', 'img/foto2.jpg', 'img/foto3.jpg'];
-let pagina = 1;
 
 // Funciones
 
-function activarAutoPlay() {
-    //Si no existe
+function activarAutoplay () {
+    // Si no existe
     if (intervalo === null) {
-        intervalo = setInterval (avanzarFoto, tiempoIntervaloSeg * 1000);
+        //intervalo = setInterval(avanzarFoto, tiempoIntervaloSeg * 1000);
+        intervalo = setInterval(function () {
+            avanzarFoto();
+        }, tiempoIntervaloSeg * 1000);
     }
+}
+
+function desactivarAutoplay () {
+    clearInterval(intervalo);
+    intervalo = null;
 }
 
 function cambiarPagina (nuevaPagina) {
@@ -46,8 +54,9 @@ function retrocederFoto () {
 function render () {
     // Imagen
     objetoImg.setAttribute('src', imagenes[pagina - 1]);
-    // Circulitos
+    // Borramos los circulitos
     circulos.textContent = '';
+    // Creamos todos los circulitos
     imagenes.forEach(function (imagen, indice) {
         // Creamos
         const nuevoCirculo = templateCirculo.cloneNode(true);
@@ -65,11 +74,10 @@ function render () {
 }
 
 // Eventos
-
 botonAvanzar.addEventListener('click', avanzarFoto);
 botonRetroceder.addEventListener('click', retrocederFoto);
-botonAutoPlay.addEventListener('click', activarAutoPlay);
-botonParar.addEventListener('click', desactivarAutoPlay);
+botonAutoplay.addEventListener('click', activarAutoplay);
+botonParar.addEventListener('click', desactivarAutoplay);
 
 // Inicio
 render();
